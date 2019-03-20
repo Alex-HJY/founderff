0f246da48a87aa7e54233d56bdd9419874952eeb
alex <156979387@qq.com>
2019-03-20 10:35:33 +0800

    n

--- web_position_excle/views.py
+++ web_position_excle/views.py
@@ -4,12 +4,12 @@ Created on 2019/3/12,11:20 AM
 
 By Alex_HJY
 """
-import matplotlib.pyplot as plt
+
 from django.shortcuts import render
 from copy import deepcopy
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
+
 from datetime import timedelta
 from dateutil import parser
 from datetime import datetime as dt
@@ -19,8 +19,6 @@ from web_position_excle.migrations import ETF100联结头寸表DATA
 from web_position_excle.migrations import 保险分级头寸表DATA
 from web_position_excle.migrations.update_excle import update_data
 
-plt.rcParams['font.sans-serif'] = ['STHeiti']
-plt.rcParams['axes.unicode_minus'] = False
 
 
 def index(request):
