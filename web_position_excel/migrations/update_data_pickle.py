64006ae641c5b1de7adcb135130f5c77525bfed0
huangjy <156979387@qq.com>
2019-03-29 09:24:01 +0800

    final

--- web_position_excel/migrations/update_data_pickle.py
+++ web_position_excel/migrations/update_data_pickle.py
@@ -16,33 +16,40 @@ from dateutil import relativedelta
 import cx_Oracle as oracle
 from decimal import Decimal
 import pickle
+import sys
+import codecs
 
+today = (datetime.now().date() - relativedelta.relativedelta(days=1)).__str__()
+today = '2019-3-12'
+yestoday = '2019-3-11'
+tomorrow = '2019-3-13'
+print(today)
 
 def update_baoxian_data(cursor):
-    today=(datetime.now().date()-relativedelta.relativedelta(days=1)).__str__()
-    cursor.execute('select sysdate from dual')
-    cursor.fetchall()
 
     data_dict = {
-        '保险分级银行存款': 95049944.40,
-        '保险分级证券清算款': -34646412.69,
+        '保险分级银行存款': get_data_oracle(cursor,
+                                    "select FZQCB from GZ25.T_TEVALUATIONGZ25 ty where FSETCODE ='A007' and FDATE like to_date ('" + yestoday + "','yyyy-mm-dd') and FKMBM ='1002'"),
+        '保险分级证券清算款': get_data_oracle(cursor,
+                                     "select FZQCB from GZ25.T_TEVALUATIONGZ25 ty where FSETCODE ='A007' and FDATE like to_date ('" + yestoday + "','yyyy-mm-dd') and FKMBM ='3003'"),
         '保险分级银行间债券交易': 0,
         '保险分级银行间回购交易': 0,
-        '保险分级T日赎回款': -6581415.79,
-        '保险分级T1日赎回款': -15450117.18,
-        '保险分级TA预确认赎回数据': -6326100,
+        '保险分级T日赎回款': -get_data_oracle(cursor,
+                                      "select sum(FBBAL) from  GZ25.T_FCWVCH ty where FSETCODE ='A007' and FDATE like to_date ('" + today + "','yyyy-mm-dd') and FVCHZY like '%赎回%款项' and FJD like 'J'"),
+        '保险分级T1日赎回款': -get_data_oracle(cursor,
+                                       "select sum(FBBAL) from  GZ25.T_FCWVCH ty where FSETCODE ='A007' and FDATE like to_date ('" + tomorrow + "','yyyy-mm-dd') and FVCHZY like '%赎回%款项' and FJD like 'J'"),
         '保险分级股票红利': 0,
         '保险分级冻结保证金备付金': 0,
         '保险分级冻结信批费': 0,
         '保险分级冻结管理费托管费': 0,
         '保险分级冻结预估网下申购款': 0,
-
+        '保险分级TA预确认赎回数据'  :-6326100.00,
         '保险分级交易所回购到期': 0,
         '保险分级银行间回购到期': 0,
-        '保险分级T日申购款': 10564316.88,
-        '保险分级T1日头寸': 42610215.62,
-
-        '保险分级基金净值': 568685607.62,
+        '保险分级T日申购款': get_data_oracle(cursor,
+                                     "select FBBAL from    GZ25.T_FCWVCH ty where FSETCODE ='A007' and FDATE like to_date ('" + today + "','yyyy-mm-dd') and FVCHZY like '%申购款项%' and FJD like 'J'"),
+        '保险分级基金净值': get_data_oracle(cursor,
+                                    "select FZQSZ from    GZ25.T_TEVALUATIONGZ25 ty where FSETCODE ='A007' and FDATE like to_date('" + yestoday + "','yyyy-mm-dd') and FKMBM like '701基金资产净值%'"),
         '保险分级活期存款保证': 6.01,
 
     }
@@ -54,6 +61,9 @@ def update_baoxian_data(cursor):
                             data_dict['保险分级股票红利'] + data_dict['保险分级冻结保证金备付金'] + data_dict['保险分级冻结管理费托管费'] \
                             + data_dict['保险分级冻结信批费'] + data_dict['保险分级冻结预估网下申购款'] + data_dict['保险分级交易所回购到期'] \
                             + data_dict['保险分级银行间回购到期']
+
+    data_dict['保险分级T1日头寸'] = data_dict['保险分级T日头寸'] + data_dict['保险分级交易所回购到期'] + data_dict['保险分级银行间回购到期'] \
+                             + data_dict['保险分级T日申购款']
 
     data_dict['保险分级T日活期存款正算'] = data_dict['保险分级银行存款'] + data_dict['保险分级证券清算款'] + \
                                 data_dict['保险分级T日赎回款'] + \
@@ -70,6 +80,9 @@ def update_baoxian_data(cursor):
                                  data_dict['保险分级T日赎回款'] + data_dict['保险分级T1日赎回款'] + \
                                  data_dict['保险分级股票红利'] + data_dict['保险分级冻结保证金备付金'] + \
                                  + data_dict['保险分级银行间回购到期'] + data_dict['保险分级交易所回购到期'] + data_dict['保险分级T日申购款']
+
+    # data_dict['保险分级TA预确认赎回数据']= data_dict['保险分级T日活期存款正算'] - data_dict['保险分级T1日头寸'] - data_dict['保险分级冻结信批费'] \
+    #                             - data_dict['保险分级冻结管理费托管费'] - data_dict['保险分级冻结预估网下申购款']
 
     data_dict['保险分级至少留活期存款'] = data_dict['保险分级基金净值'] * data_dict['保险分级活期存款保证'] / Decimal('100')
 
@@ -88,12 +101,14 @@ def update_baoxian_data(cursor):
 
 def update_ETF100_data(cursor):
     data_dict = {
-        'ETF100银行存款': 3168681.57,
-        'ETF100清算款': -1290298.17,
-        'ETF100银行间回购交易': 0,
+        'ETF100银行存款':get_data_oracle(cursor,
+                                    "select FZQCB from GZ25.T_TEVALUATIONGZ25 ty where FSETCODE ='A015' and FDATE like to_date ('" + yestoday + "','yyyy-mm-dd') and FKMBM ='1002'"),
+        'ETF100清算款':get_data_oracle(cursor,
+                                     "select FZQCB from GZ25.T_TEVALUATIONGZ25 ty where FSETCODE ='A015' and FDATE like to_date ('" + yestoday + "','yyyy-mm-dd') and FKMBM ='3003'"),
+         'ETF100银行间回购交易': 0,
         'ETF100交易间回购交易': 0,
         'ETF100T日赎回款': -682644.01,
-        'ETF100T1日赎回款': -1513704.24,
+      'ETF100T1日赎回款': -1513704.24,
         'ETF100债券付息': 0,
         'ETF100冻结保证金备付金': 0,
         'ETF100冻结管理费托管费': 0,
@@ -106,7 +121,8 @@ def update_ETF100_data(cursor):
 
         'ETF100T1日活期存款倒算': 0,
         'ETF100T1日活期存款正算': 0,
-        'ETF100基金净值': 28807973.93,
+        'ETF100基金净值': get_data_oracle(cursor,
+                                    "select FZQSZ from    GZ25.T_TEVALUATIONGZ25 ty where FSETCODE ='A015' and FDATE like to_date('" + yestoday + "','yyyy-mm-dd') and FKMBM like '701基金资产净值%'"),
         'ETF100活期存款保证': 6.01,
         'ETF100至少留活期存款': 0,
         'ETF100日日终活期存款估计': 0,
@@ -139,10 +155,22 @@ def update_ETF100_data(cursor):
     return data_dict
 
 
+def get_data_oracle(cursor, ss):
+    cursor.execute(ss)
+    data = cursor.fetchall()
+    if len(data) != 0:
+        return data[0][0]
+    else:
+        return 0
+
+
 def update_data():
+    import os
+    os.environ["NLS_LANG"] = ".AL32UTF8"
     # connect oracle database
+
     try:
-        db = oracle.connect('scott/redhat@192.168.223.138:1521/oracle.test')
+        db = oracle.connect('FDC/fdc@172.16.109.97:5003/ttapdb01')
     except Exception as e:
         print(e)
         db = ''
@@ -155,11 +183,12 @@ def update_data():
 
         dict1 = update_baoxian_data(cursor)
         dict2 = update_ETF100_data(cursor)
-
-        # close cursor and oracle
         cursor.close()
         db.close()
 
         return dict1, dict2
     else:
         return
+
+
+update_data()
